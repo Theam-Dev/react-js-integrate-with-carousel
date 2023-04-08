86f5c8b71989d0a477b663f232ddf0274ea74908
theam-dev <phearakroun@gmail.com>
2023-04-08 19:56:34 +0700

    Carousel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
+import p1 from './assets/p1.jpg';
+import p2 from './assets/p2.jpg';
+import p3 from './assets/p7.jpg';
+import p4 from './assets/p6.jpg';
+import p5 from './assets/p5.jpg';
+class App extends React.Component {
+  state = {
+    images: [
+      p1,p2,p3,p4,p5
+    ]
+  }
+  render(){
 
-function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+
+    <Carousel autoPlay>
+      {this.state.images.map((post) => (
+    <div >
+        <img src={post} />
     </div>
+    ))}
+    
+</Carousel>
   );
+}
 }
 
 export default App;
